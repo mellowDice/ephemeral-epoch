5139214ca1fb5f85c1e5f3b4e5e3cdaba7509533
Timothy Do <timothy.do@gmail.com>
2016-06-20 16:41:11 -0700

    Adding Unity .cs assets

---
+++ ee_client/Client Assets/NetworkMove.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class NetworkMove : MonoBehaviour {
+
+  public SocketIOComponent socket;
+
+  public void OnMove (Vector3 position) {
+    Debug.Log("sending: " + position);
+    socket.Emit("move");
+  }
+}
