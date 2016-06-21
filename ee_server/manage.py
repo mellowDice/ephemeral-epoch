0b0f5c734c5e19353087e728979ac6c97b214f25
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-06-20 20:19:59 -0700

    latest server changes

--- ee_server/manage.py
+++ ee_server/manage.py
@@ -7,30 +7,33 @@ app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
 user_count = 0
+all_users = []
+# store all at current time
 
 @app.route('/')
 def index():
-    return 'You did it'
+    return 'Welcome to Ethereal Epoch'
 
 # Setup
 def store_user():
     print(request.sid) # user's session id
     # Connect DB and store user
     user_count += 1 # User id placeholder
+    # each has an individual spawn event
     return user_count
 
 @socketio.on('connect')
 def test_connect():
     print(request.sid)
-    emit('my response', {'data': 'Connected'}, callback=store_user)
+    return store_user()
 
 # Broadcast to client
 @socketio.on('user_movement')
 def handle_user_movement(json):
     print('received json: ' + str(json))
-    # Call service
-    emit('my response', json, broadcast=True)#broadcasts messages to user. also works with send
-    share_user_movement(json)
+    # Call service 
+    #broadcasts messages to user. also works with send
+    return share_user_movement(json)
 
 # request landscape
 @socketio.on('request_landscape')
@@ -41,22 +44,18 @@ def handle_landscape_request(json):
 def share_user_movement(json): 
     print('send user movement to other users' + str(json))
     #Get users from DB and send data to each
-    emit('my response', json, namespace='/')
+    emit('move', json, namespace='/')
 
 # Listen for client data
 
-@socketio.on('message') # String data
-def handle_message(message):
-    print('received message: ' + message)
-
 @socketio.on('json') # JSON data
-def handle_json(json):
+def handle_user_direction(json):
     print('received json: ' + str(json))
 
 # disconnect 
 
-@socketio.on('disconnect', namespace='/chat')
-def test_disconnect():
+@socketio.on('disconnect')
+def disconnect():
     print('Client disconnected')
     user_count -= 1
     return user_count
