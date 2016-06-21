eb951333f88954e4c97fb11434b18dfedbc9db83
Timothy Do <timothy.do@gmail.com>
2016-06-20 20:17:50 -0700

    Working test client

--- ee_client/Client Assets/NetworkController.cs
+++ ee_client/Client Assets/NetworkController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using SocketIO;
 
 public class NetworkController : MonoBehaviour {
@@ -7,29 +8,58 @@ public class NetworkController : MonoBehaviour {
 	// Use this for initialization
   static SocketIOComponent socket;
   public GameObject playerPrefab;
+
+  Dictionary<string, GameObject> players;
   
   void Awake () {
     socket = GetComponent<SocketIOComponent>();
   }
 	void Start () {
-   socket.On("open", OnConnected);
-   // socket.On("buildterrain", BuildTerrain);
-   socket.On("spawn", OnSpawned);
+    // socket.On("abcd", BuildTerrain);
+    socket.On("open", OnConnected);
+    socket.On("spawn", OnSpawned);
+    socket.On("clientDisconnect", OnDisconnect);
+    socket.On("move", OnMove);
+
+    players = new Dictionary<string, GameObject> ();
 	}
 
   void OnConnected(SocketIOEvent e) {
     Debug.Log("Connected to server.");
-    socket.Emit("loggedIn");
+    // socket.Emit("loggedIn");
   }
 
   void OnSpawned(SocketIOEvent e) {
-    Debug.Log("New Player Spawned");
-    Instantiate(playerPrefab);
+    Debug.Log("New Player Spawned" + e.data);
+    var player = Instantiate(playerPrefab);
+    players.Add(e.data["id"].ToString(), player);
+
+    Debug.Log("count: " + players.Count);
   }
 
   void BuildTerrain(SocketIOEvent e) {
     Debug.Log("Building Terrain...");
-    Debug.Log(e);
+    // BuildingTerrain
 
+    // Debug.Log(e);
+  }
+
+  void OnDisconnect(SocketIOEvent e) {
+    Debug.Log("Client disconnected... " + e.data);
+    var id = e.data["id"].ToString();
+    var player = players[id];
+    Destroy(player);
+    players.Remove(id);
+  }
+
+  float GetJSONFloat (JSONObject data, string key) {
+    return float.Parse(data[key].ToString().Replace("\"", ""));
+  }
+
+  void OnMove(SocketIOEvent e) {
+    var player = players[e.data["id"].ToString()];
+    var navigate = player.GetComponent<NavigatePosition>();
+    var pos = new Vector3(GetJSONFloat(e.data, "x"), GetJSONFloat(e.data, "y"), GetJSONFloat(e.data, "z"));
+    navigate.NavigateTo(pos);
   }
 }
--- ee_client/Client Assets/NetworkMove.cs
+++ ee_client/Client Assets/NetworkMove.cs
@@ -7,7 +7,11 @@ public class NetworkMove : MonoBehaviour {
   public SocketIOComponent socket;
 
   public void OnMove (Vector3 position) {
-    Debug.Log("sending: " + position);
-    socket.Emit("move");
+    Debug.Log("sending: " + VectorToJSON(position));
+    socket.Emit("move", new JSONObject(VectorToJSON(position)));
+  }
+
+  string VectorToJSON (Vector3 position) {
+    return string.Format(@"{{""x"":""{0}"", ""y"":""{1}"", ""z"":""{2}""}}", position.x, position.y, position.z);
   }
 }
