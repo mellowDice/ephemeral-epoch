3571b1a7984fa904a8b407c2348206363a60b88b
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-06-21 19:58:57 -0700

    Server with eventlet monkey patch

--- ee_server/manage.py
+++ ee_server/manage.py
@@ -1,18 +1,17 @@
 import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, send, emit, join_room
-from threading import Thread
 from ee_modules.landscape.fractal_landscape import fractal_landscape
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
 all_users = []
 user_count = 0
-# store all at current time
+
 
 @app.route('/hello')
 def index():
@@ -21,12 +20,9 @@ def index():
 # Setup
 def send_new_user_data():
     emit('load', {'users': all_users, 'terrain':fractal_landscape(300, 300, 300, 300, 4)}, room=request.sid) # emits just to new connecting user
-    print('id available in callback', request.sid) # user's session id
 
 @socketio.on('connect')
 def test_connect():
-    # Does a logged in user get the ids of every user on the page? 
-    # Does a second user send a new spawn event to every other user?
     global all_users, user_count
     print('connect with socket info', request.sid)
     user_count += 1
@@ -37,8 +33,7 @@ def test_connect():
 @socketio.on('move')
 def share_user_movement(json): 
     print('send user movement to other users' + str(json) + request.sid)
-    #Get users from DB and send data to each
-    emit('move', {'id': request.sid, 'coordinates': json}, broadcast=True)
+    emit('playerMove', {'id': request.sid, 'coordinates': str(json)}, broadcast=True)
 
 # disconnect 
 
@@ -49,8 +44,8 @@ def disconnect():
     user_count -= 1
     all_users.remove(request.sid)
     emit('onEndSpawn', {'id': request.sid}, broadcast=True) # currently doens't de-render user
-# error handling
 
+# error handling
 @socketio.on_error()    
 def error_handler(e):
     print('error', e)
@@ -66,24 +61,3 @@ if __name__ == '__main__':
     # socketio.run(app)
     eventlet.wsgi.server(eventlet.listen(('', 6000)), app, debug=True)
 
-
-
-# @app.route('/', methods=['GET']) # replace with socket
-# def load():#spawn broadcast
-# #loaded
-# #sessions
-#     # call map generation service
-#     return 'Map sent to client'
-
-# @app.route('/friends')
-# def getfriends():
-#     # call to DB
-#     return 'Friends sent to client'
-
-
-
-# # request landscape
-# @socketio.on('request_landscape')
-# def handle_landscape_request(json):
-#     print('received landscape request')
-#     emit('landscape_matrix', {data: fractal_landscape(300, 300, 300, 300, 8)})
