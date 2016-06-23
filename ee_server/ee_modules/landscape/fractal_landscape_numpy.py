7896dfd75b38c3ad02548b5e84b54d030b281381
Austin Baltes <austinba@gmail.com>
2016-06-22 20:52:11 -0700

    made progress on fractal landscape in matrixes

--- ee_server/ee_modules/landscape/fractal_landscape_numpy.py
+++ ee_server/ee_modules/landscape/fractal_landscape_numpy.py
@@ -6,7 +6,7 @@ def create_landscape(width, height, period_width, period_height):
     coarse_i_count = width // period_width + 1
     coarse_j_count = height // period_height + 1
 
-    # coarse grid consists of 2D matrix, with submatrix [[upper-left, upper-right], [bottom-left, bottom-right]], with subarray [vector-x, vector-y] of random unit vectors
+    # The 2D course-grid matrix of random unit vectors, which defines the grid, with submatrix [[upper-left, upper-right], [bottom-left, bottom-right]], with subarray [vector-x, vector-y]
     coarse_grid_angles = np.random.rand(coarse_i_count, coarse_j_count)
     coarse_grid_vectors = np.array([np.cos(coarse_grid_angles), np.sin(coarse_grid_angles)]).transpose(1, 2, 0)
     coarse_grid_vectors_4corners = np.array([[coarse_grid_vectors,] * 2,] * 2).transpose(3, 4, 0, 1, 2)
@@ -15,20 +15,28 @@ def create_landscape(width, height, period_width, period_height):
     fine_grid_vectors_4corners = np.repeat(np.repeat(coarse_grid_vectors_4corners, period_width, axis=0), period_height, axis=1)
 
     # create a left-to-right gradient in fine grid dimensions
-    fine_grid_template = np.array([np.linspace(1, 0, period_width+1),] * (period_height+1))
-    print(fine_grid_template)
+    fine_grid_gradient_left = np.array([np.linspace(1, 0, period_width+1),] * (period_height+1))
+    fine_grid_gradient_up = fine_grid_gradient_left.transpose()
     
-    # The 2D fine-grid weighting matrix, with submatrix [[upper-left, upper-right], [bottom-left, bottom-right]], and subarray [vector-x, vector-y] (vector-x = vector-y)
-    weighting_fine_grid = fine_grid_template * fine_grid_template.transpose()
-    weighting_fine_grid = np.array([[[weighting_fine_grid[:-1,:-1], weighting_fine_grid[:-1,::-1][:,:-1]],
-                                     [weighting_fine_grid[::-1,:-1][:-1,:], weighting_fine_grid[::-1,::-1][:-1,:-1]]],] * 2).transpose(3, 4, 1, 2, 0)
-    weighting_fine_grid = np.tile(weighting_fine_grid, (coarse_i_count, coarse_j_count))
+    # The 2D fine-grid weighting matrix for interpolation, with submatrix [[upper-left, upper-right], [bottom-left, bottom-right]], and vector subarray [vector-x, vector-y] (vector-x = vector-y)
+    weighting_fine_grid = fine_grid_gradient_left * fine_grid_gradient_up
+    weighting_fine_grid = np.array([
+                                    [
+                                     [weighting_fine_grid[:-1  , :-1], weighting_fine_grid[:-1  , :0:-1]],  # upper-left and upper-right weightings
+                                     [weighting_fine_grid[:0:-1, :-1], weighting_fine_grid[:0:-1, :0:-1]]   # lower-left and lower-right weightings
+                                    ],] * 2
+                                   ).transpose(3, 4, 1, 2, 0)
+    # weighting_fine_grid = np.tile(weighting_fine_grid, (coarse_i_count, coarse_j_count))
 
-    vectors_fine_grid = np.array([fine_grid_template, fine_grid_template]).transpose(1, 2, 0)
-    print(vectors_fine_grid)
+    # The 2D fine-grid vectors matrix for determining height (by multiplying vs. coarse-grid vectors), with submatrix [[upper-left, upper-right], [bottom-left, bottom-right]], and vector subarray [vector-x, vector-y]
+    vectors_fine_grid = np.array( 
+                            [[[fine_grid_gradient_left[:-1, :0:-1], fine_grid_gradient_up[:0:-1, :-1]],   # upper-left
+                              [fine_grid_gradient_left[:-1, :-1]  , fine_grid_gradient_up[:0:-1, :-1]]],  # upper-right
+                             [[fine_grid_gradient_left[:-1, :0:-1], fine_grid_gradient_up[:-1  , :-1]],   # lower-left
+                              [fine_grid_gradient_left[:-1, :-1]  , fine_grid_gradient_up[:-1  , :-1]]]]  # lower-right
+                        ).transpose(3, 4, 0, 1, 2)
 
-
-
+    print(fine_grid.shape)
     # print(fine_grid_weighting)
 
 
